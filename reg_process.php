8e978f3bf2b5f89fcb36edf281dfca2f20b6214f
Vernon-C <vernonon0530@gmail.com>
2021-05-06 17:41:42 +0800

    Fix code in reg_process

--- reg_process.php
+++ reg_process.php
@@ -120,8 +120,7 @@
                                             // echo $_POST['input_nric'];
                                             if (file_exists($_POST['input_nric'] . '.txt')) {     //Check for filename with given nric
                                                 echo '<h3>You are already registered!</h3>';
-                                            }
-                                            else {  //filename not found, write to file
+                                            } else {  //filename not found, write to file
                                                 $content_to_write = "Name: " . $_POST['input_name'] . "\n";
                                                 $content_to_write .= "NRIC: " . $_POST['input_nric'] . "\n";
                                                 file_put_contents($_POST['input_nric'] . '.txt', $content_to_write);
